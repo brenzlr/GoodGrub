99964243b976c3447bfaf92ada61e6e4ecd43cd3
brenzlr <brenzlr@users.noreply.github.com>
2022-06-16 22:40:53 -0400

    css affecting the entire page, header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,31 +5,66 @@ import { LoggedInContext } from "../App";
 import { useContext } from "react";
 import { LogoutButton } from "../components/LogoutButton";
 
+import {
+  MDBNavbar,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBNavbarToggler,
+  MDBContainer,
+  MDBIcon,
+} from "mdb-react-ui-kit";
 
 export function Header() {
   const [isLoggedIn, setIsLoggedIn] = useContext(LoggedInContext);
 
   return (
-    <div>
-      <div>{isLoggedIn ? "Welcome" : "Please login"}</div>
-      <div style={{ display: "flex" }}>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <NavButton to="/" label="Home" />
-          <NavButton to="/about" label="About" />
-          <NavButton to="/contact" label="Contact" />
-        </div>
-        <div style={{ display: "flex", flexDirection: "row" }}>
-          {isLoggedIn ? (
-            <LogoutButton />
-          ) : (
-            <>
-              <NavButton to="/login" label="Login" />
-              <NavButton to="/register" label="Register" />
-            </>
-          )}
-        </div>
-      </div>
-    </div>
+    <header>
+      <MDBNavbar expand="lg" light bgColor="white" fixed>
+        <MDBContainer fluid>
+          <MDBNavbarToggler
+            aria-controls="navbarExample01"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <MDBIcon fas icon="bars" />
+          </MDBNavbarToggler>
+          <div className="collapse navbar-collapse" id="navbarExample01">
+            <MDBNavbarNav right className="mb-2 mb-lg-0">
+              <MDBNavbarItem active>
+                <MDBNavbarLink aria-current="page" href="/">
+                  Home
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/about">About</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href="/contact">Contact</MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                {isLoggedIn ? (
+                  <LogoutButton />
+                ) : (
+                  <>
+                    <MDBNavbarLink href="/login">Login</MDBNavbarLink>
+                  </>
+                )}
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                {isLoggedIn ? (
+                  ""
+                ) : (
+                  <>
+                    <MDBNavbarLink href="/register">Register</MDBNavbarLink>
+                  </>
+                )}
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </div>
+        </MDBContainer>
+      </MDBNavbar>
+    </header>
   );
 }
 
