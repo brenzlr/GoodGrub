497d37ead04f957e33f38932c7493d4e19b06c8a
glenpham <43647463+glenpham@users.noreply.github.com>
2022-06-22 14:32:26 -0400

    MyRecipe page Complete

--- src/pages/MyRecipes.js
+++ src/pages/MyRecipes.js
@@ -1,22 +1,41 @@
 import React from "react";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import "../components/RecipeCard.css";
-import { RecipeData } from "../components/RecipeData";
 import { ThemeContext } from "App";
 import { LoggedInContext } from "../App";
+import { useFetch } from "hooks/useFetch";
+import RecipeCard from "components/RecipeCard";
 
 export function MyRecipes() {
   const [isLoggedIn, setIsLoggedIn] = React.useContext(LoggedInContext);
   const [theme, setTheme] = useContext(ThemeContext);
 
+  const [recipeData] = useFetch(
+    "http://localhost:3001/myrecipes/" + isLoggedIn,
+    "GET"
+  );
+
+  console.log(recipeData);
+
+  const recipeList =
+    recipeData &&
+    recipeData.map((recipe) => (
+      <div key={recipe.recipeName} >
+        <RecipeCard title={recipe.recipeName} 
+        imgUrl={recipe.imgUrl}
+        description={recipe.recipeInstructions}
+        ingredients={recipe.recipeIngredients}
+        type={recipe.recipeType}
+        duration={recipe.recipeDuration}/>
+      </div>
+    ));
+
   return (
     <div id={theme}>
       <br></br>
-      <h1 style={{ textAlign: "center" }}>My Recipes</h1>;
+      <h1 style={{ textAlign: "center" }}>My Favorites</h1>;
       <div className="body">
-        <div className="wrapper">
-          <RecipeData username={isLoggedIn.username} />
-        </div>
+        <div className="wrapper">{recipeList}</div>
       </div>
     </div>
   );
