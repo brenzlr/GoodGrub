cb451e25ecb7b929ec775e434fb53289e3ceb50e
glenpham <43647463+glenpham@users.noreply.github.com>
2022-06-22 14:37:55 -0400

    Logout Button redirect to Home

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { LoggedInContext } from "../App";
+import { useNavigate } from "react-router-dom";
 
 export function LogoutButton() {
   const [isLoggedIn, setIsLoggedIn] = React.useContext(LoggedInContext);
+  const navigate = useNavigate();
+
   return (
     <div>
       <button
         onClick={() => {
           setIsLoggedIn(false);
           alert("Logged out");
+          navigate("/");
         }}
       >
         Logout
